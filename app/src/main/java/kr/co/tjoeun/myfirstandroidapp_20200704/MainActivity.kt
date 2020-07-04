f19d87c02ffe8a87ecc81dfa12b453a34be2dd3a
황선혜 <ghkdtjs6451@gmail.com>
2020-07-04 11:49:42 +0900

    .

---
+++ app/src/main/java/kr/co/tjoeun/myfirstandroidapp_20200704/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.myfirstandroidapp_20200704
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
